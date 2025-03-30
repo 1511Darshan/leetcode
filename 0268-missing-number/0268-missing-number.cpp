708832187bb9c311be5021355dd682022f9fad1c
Darshan <rushdarshan@gmail.com>
2025-03-30 12:23:55 +0530

    Time: 0 ms (100%), Space: 21.8 MB (39.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,8 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
         int res = nums.size();
         for(int i = 0; i< nums.size();i++){
-            res ^= i;
-            res ^= nums[i];
+            res = res ^ i ^ nums[i];
         }
         return res;
     }
