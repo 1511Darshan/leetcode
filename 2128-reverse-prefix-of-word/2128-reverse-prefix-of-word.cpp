8786cf644e7186d74e968108f10861c6b9f00454
Darshan <rushdarshan@gmail.com>
2025-04-01 13:09:32 +0530

    Time: 0 ms (100%), Space: 8.4 MB (69.74%) - LeetHub

---
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int chi = word.find(ch);
+        reverse(word.begin(),word.begin() + chi + 1);
+        return word;
+    }
+};
