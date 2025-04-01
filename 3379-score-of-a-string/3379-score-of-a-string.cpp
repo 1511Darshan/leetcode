f770bc300d7d997bcf76010e2c46cb1496d27ad3
Darshan <rushdarshan@gmail.com>
2025-04-01 11:52:32 +0530

    Time: 0 ms (100%), Space: 8.8 MB (64.45%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.cpp
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int c = 0;
-        for(int i = 0; i<s.size() -1; ++i){
-            c += abs(s[i] - s[i+1]);
-        }
-        return c;
+       int score = 0;
+       for(int i = 0; i<s.size() - 1;++i){
+            score += abs(s[i] - s[i+1]);
+       }
+       return score; 
     }
 };
