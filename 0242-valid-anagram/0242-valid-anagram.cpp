75892df01808c5e0d543b7e821a9733c0379f81d
Darshan <rushdarshan@gmail.com>
2025-11-09 06:49:47 +0530

    Time: 3 ms (51.12%), Space: 9.7 MB (73.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()){
+        if(s.size() != t.size()){
             return false;
         }
-        unordered_map<char,int> freqS;
-        unordered_map<char,int> freqT;
-        for(int i = 0; i<s.length();i++){
-            freqS[s[i]]++;
-            freqT[t[i]]++;
+        vector<int> charcount(26,0);
+        for(int i = 0; i< s.size(); ++i){
+            charcount[s[i] - 'a']++;
+            charcount[t[i] - 'a']--;
         }
-        return freqS == freqT;
+        for(int count : charcount){
+            if(count != 0){
+                return false;
+            }
+        }
+        return true;
     }
 };
