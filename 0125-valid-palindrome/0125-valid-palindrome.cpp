5fbd03211b741be4f70ba60713612a865ae82335
Darshan <rushdarshan@gmail.com>
2026-01-24 16:59:01 +0530

    Time: 0 ms (100%), Space: 10 MB (54.32%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int start = 0;
-        int end = s.size() - 1;
-        while(start<= end){
+        int end = s.size()-1;
+        while(start <= end){
             if(!isalnum(s[start])){
                 start++;
                 continue;
@@ -15,11 +15,11 @@ public:
             if(tolower(s[start]) != tolower(s[end])){
                 return false;
             }
-            else{
-                start++;
-                end--;
-            }
+            start++;
+            end--;
         }
         return true;
+        
+        
     }
 };
